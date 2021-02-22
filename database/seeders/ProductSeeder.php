00060e6b6ad77ddd4bec5d92fe04c0edbff6f4cc
Rronlluka03 <rl35360@ubt-uni.net>
2021-02-22 01:36:13 +0100

    7th

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -40,14 +40,21 @@ class ProductSeeder extends Seeder
             "description"=>"A smartv with much more features",
             "category"=>"tv",
             "gallery"=>"https://images-na.ssl-images-amazon.com/images/I/91v4nFq0LuL._SL1500_.jpg",
-        ],*/
+        ],
         [
             'name'=>'LG Fridge',
             "price"=>"600",
             "description"=>"All teh feautures",
             "category"=>"fridge",
             "gallery"=>"https://www.mime.asia/wp-content/uploads/2020/11/RefrigeratorStorage.jpg",
-        ],
+        ],*/
+        [
+            'name'=>'OPPO',
+            "price"=>"300",
+            "description"=>"A smartphone with 4gb ram and much more features",
+            "category"=>"Mobile",
+            "gallery"=>"https://5.imimg.com/data5/CZ/RS/ZT/SELLER-100534408/oppo-mobile-phones-f15-500x500.jpg",
+        ]
     );
 
     }
